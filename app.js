7fe9af243428794c00879ca1440633c93a93047b
Oct4v33an <67186273+Oct4v33an@users.noreply.github.com>
2021-03-31 21:09:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-﻿const toggleBtn = document.querySelector(".sidebar-toggle");
+﻿
+
+//sidebar
+const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
